601a75c2ef0648a1cff0ea9ebd32727d8d6b3a7b
iliana etaoin <iliana@buttslol.net>
2021-10-05 13:32:41 -0700

    changes

--- build-iso.sh
+++ build-iso.sh
@@ -9,8 +9,7 @@ cp artifacts/player-x86_64-pc-windows-msvc/player.exe voyager/Voyager-Windows.ex
 cp artifacts/player-universal-apple-darwin/player voyager/Voyager-macOS
 chmod a+x voyager/Voyager-{Linux-{x86_64,aarch64},macOS}
 
-# shellcheck disable=SC2154
-skopeo copy "docker://ghcr.io/lc-41/tapes@$tapes_digest" oci:tapes
+skopeo copy "docker://ghcr.io/lc-41/tapes@$TAPES_DIGEST" oci:tapes
 manifest="$(jq -r '.manifests[].digest | split(":")[1]' tapes/index.json)"
 blob="$(jq -r '.layers[0].digest | split(":")[1]' "tapes/blobs/sha256/$manifest")"
 tar -C voyager/data -xvf "tapes/blobs/sha256/$blob"
---
+++ build-static.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -euxo pipefail
+
+cd blaseball.vcr
+git-restore-mtime
+
+mkdir -p .cargo
+# run `cargo vendor` in a container as a workaround for https://github.com/rust-lang/cargo/issues/8443
+docker run --rm -u "$(id -u):$(id -g)" -v "$PWD:/work" -w work \
+    "rust:$RUST_VER" cargo vendor > .cargo/config.toml
+hardlink vendor
+# clamp vendor mtimes at Cargo.lock's mtime
+find vendor -newer Cargo.lock -print0 | xargs -0r touch --no-dereference --reference=Cargo.lock
+
+pushd vendor/before
+zip -9qr static.zip static
+rm -rf static/*
+echo "see static.zip elsewhere on the Voyager disc" > static/README
+popd
+mv vendor/before/static.zip ../
+
+echo "see zstd-dictionaries elsewhere on the Voyager disc" > zstd-dictionaries/README
+unset POSIXLY_CORRECT # this should be the case but do it just in case
+shopt -s dotglob
+tar --sort=name --owner=0 --group=0 --numeric-owner \
+    --exclude=.git --exclude="zstd-dictionaries/*.dict" \
+    -cf ../source.tar -- *
+shopt -u dotglob
+zstd -q -19 ../source.tar
